89c6731428ce15a59d9be328cb35941f01f62262
mukhamadkurniarrokhman-del <mukhamadkurniarrokhman@gmail.com>
2026-04-24 20:41:24 +0700

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,23 +6,20 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
-    ->withRouting(
-        web: __DIR__ . '/../routes/web.php',
-        api: __DIR__ . '/../routes/api.php',
-        commands: __DIR__ . '/../routes/console.php',
-        health: '/up',
-    )
-    ->withMiddleware(function (Middleware $middleware) {
-        // Aktifkan CORS global untuk API (baca dari config/cors.php)
-        $middleware->append(\Illuminate\Http\Middleware\HandleCors::class);
+        ->withRouting(
+                    web: __DIR__ . '/../routes/web.php',
+                    api: __DIR__ . '/../routes/api.php',
+                    commands: __DIR__ . '/../routes/console.php',
+                    health: '/up',
+                )
+        ->withMiddleware(function (Middleware $middleware) {
+                    $middleware->append(\Illuminate\Http\Middleware\HandleCors::class);
 
-        $middleware->statefulApi();
-
-        $middleware->alias([
-            'admin' => EnsureIsAdmin::class,
-        ]);
-    })
-    ->withExceptions(function (Exceptions $exceptions) {
-        //
-    })
-    ->create();
+                                 $middleware->alias([
+                                                                'admin' => EnsureIsAdmin::class,
+                                                            ]);
+        })
+        ->withExceptions(function (Exceptions $exceptions) {
+                    //
+        })
+        ->create();
